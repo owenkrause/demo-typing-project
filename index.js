15a1acee115900ad7828647d882cbf41f78e5fc1
owenkrause <owenjkrause@gmail.com>
2024-02-20 18:28:51 -0800

    workshop #2

--- index.js
+++ index.js
@@ -1,6 +1,250 @@
+const wordList = [
+    "the",
+    "be",
+    "of",
+    "and",
+    "a",
+    "to",
+    "in",
+    "he",
+    "have",
+    "it",
+    "that",
+    "for",
+    "they",
+    "I",
+    "with",
+    "as",
+    "not",
+    "on",
+    "she",
+    "at",
+    "by",
+    "this",
+    "we",
+    "you",
+    "do",
+    "but",
+    "from",
+    "or",
+    "which",
+    "one",
+    "would",
+    "all",
+    "will",
+    "there",
+    "say",
+    "who",
+    "make",
+    "when",
+    "can",
+    "more",
+    "if",
+    "no",
+    "man",
+    "out",
+    "other",
+    "so",
+    "what",
+    "time",
+    "up",
+    "go",
+    "about",
+    "than",
+    "into",
+    "could",
+    "state",
+    "only",
+    "new",
+    "year",
+    "some",
+    "take",
+    "come",
+    "these",
+    "know",
+    "see",
+    "use",
+    "get",
+    "like",
+    "then",
+    "first",
+    "any",
+    "work",
+    "now",
+    "may",
+    "such",
+    "give",
+    "over",
+    "think",
+    "most",
+    "even",
+    "find",
+    "day",
+    "also",
+    "after",
+    "way",
+    "many",
+    "must",
+    "look",
+    "before",
+    "great",
+    "back",
+    "through",
+    "long",
+    "where",
+    "much",
+    "should",
+    "well",
+    "people",
+    "down",
+    "own",
+    "just",
+    "because",
+    "good",
+    "each",
+    "those",
+    "feel",
+    "seem",
+    "how",
+    "high",
+    "too",
+    "place",
+    "little",
+    "world",
+    "very",
+    "still",
+    "nation",
+    "hand",
+    "old",
+    "life",
+    "tell",
+    "write",
+    "become",
+    "here",
+    "show",
+    "house",
+    "both",
+    "between",
+    "need",
+    "mean",
+    "call",
+    "develop",
+    "under",
+    "last",
+    "right",
+    "move",
+    "thing",
+    "general",
+    "school",
+    "never",
+    "same",
+    "another",
+    "begin",
+    "while",
+    "number",
+    "part",
+    "turn",
+    "real",
+    "leave",
+    "might",
+    "want",
+    "point",
+    "form",
+    "off",
+    "child",
+    "few",
+    "small",
+    "since",
+    "against",
+    "ask",
+    "late",
+    "home",
+    "interest",
+    "large",
+    "person",
+    "end",
+    "open",
+    "public",
+    "follow",
+    "during",
+    "present",
+    "without",
+    "again",
+    "hold",
+    "govern",
+    "around",
+    "possible",
+    "head",
+    "consider",
+    "word",
+    "program",
+    "problem",
+    "however",
+    "lead",
+    "system",
+    "set",
+    "order",
+    "eye",
+    "plan",
+    "run",
+    "keep",
+    "face",
+    "fact",
+    "group",
+    "play",
+    "stand",
+    "increase",
+    "early",
+    "course",
+    "change",
+    "help",
+    "line"
+]
+
+const container = document.getElementById('words')
 const input = document.getElementById('input');
+console.log(container)
 console.log(input)
 
+container.addEventListener('click', e => {
+    input.focus()
+})
+
+let wordIndex = 0
+let charIndex = 0
+
+
+
+function randomWords() {
+    const words = []
+    for (let i = 0, n = 15; i < n; i++) {
+        const ran = Math.floor(Math.random() * wordList.length)
+        words.push(wordList[ran])
+    }
+    return words
+}
+
+const test = randomWords()
+
+
+for (let i = 0, n = 15; i < n; i++) {
+    const word = container.appendChild(document.createElement('div'))
+    word.style.margin = '4px'
+    word.style.cursor = 'default'
+    for (let j = 0, n = test[i].length; j < n; j++) {
+        word.appendChild(document.createElement('span')).textContent = test[i][j]
+    }
+}
+
+let wordElements = container.querySelectorAll('div')
+console.log(wordElements)
+
+
 input.addEventListener('keydown', e => {
-    console.log(e)
+    if(e.key !== test[wordIndex][charIndex]) {
+        wordElements[wordIndex].childNodes[charIndex].style.color = 'red'
+    } else if (e.key === test[wordIndex][charIndex]) {
+        wordElements[wordIndex].childNodes[charIndex].style.color = '#E0E0E0'
+    }
+    charIndex++
 })
